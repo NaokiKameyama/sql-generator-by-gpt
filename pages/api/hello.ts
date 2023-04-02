cf08c4671b7aa98f22f920001cbf75f4e171e9d0
Naoki-Kameyama <atukan0930@gmail.com>
2023-04-02 10:48:11 +0900

    環境変数の構成

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,7 +6,9 @@ import * as admin from "firebase-admin";
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FSA_PROJECT_ID,
-    privateKey: process.env.FSA_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: process.env.FSA_PRIVATE_KEY
+      ? process.env.FSA_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : process.env.FSA_PRIVATE_KEY,
     clientEmail: process.env.FSA_CLIENT_EMAIL,
   }),
 });
